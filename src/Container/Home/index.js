f6f2b05d763bbfb5e6eb2250175e12e65547f416
Ameerusa86 <engamermecha@gmail.com>
2023-01-14 15:36:49 -0500

    -

--- src/Container/Home/index.js
+++ src/Container/Home/index.js
@@ -3,18 +3,20 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import axios from "axios";
+import CardMoviesComponents from "../../Components/Card";
 
 const HomeContainer = () => {
   const API_KEY = process.env.REACT_APP_NOT_SECRET_CODE;
   const [content, setContent] = useState([]);
   const [pageno, setPageno] = useState(1);
-  const [paginationno, setPaginationsno] = useState(0);
+  const [paginationno, setPaginationno] = useState(0);
 
   const GetDataTrending = async () => {
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}&page=${pageno}`
     );
-    setContent(data);
+    setContent(data.results);
+    setPaginationno(data.total_pages);
     console.log("data", data);
   };
 
@@ -25,7 +27,23 @@ const HomeContainer = () => {
     <main className="homePage">
       <Container>
         <Row>
-          <Col className="col-12">Hello form Home Page</Col>
+          <Col className="col-12">
+            <section>
+              <h1 className="txtCenter">Top Trending </h1>
+              <h3 className="txtCenter">MOVIES & TV SERIES just for you</h3>
+            </section>
+          </Col>
+          {content && content.length > 0
+            ? content.map((item, index) => {
+                return (
+                  <CardMoviesComponents
+                    key={index}
+                    data={item}
+                    mediaType="tv"
+                  />
+                );
+              })
+            : "Loading ...."}
         </Row>
       </Container>
     </main>
